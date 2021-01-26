7f816b6a4359b49747885141ecba73afb20c7f2a
Shivam Kumar <shivam66.sk@gmail.com>
2021-01-26 17:14:42 +0530

    redux fundamentals completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+import store from './store';
+import { bugAdded, bugRemoved, bugResolved } from './actions';
+
+const unsubscribe = store.subscribe(() => {
+  console.log('Store Changed: ', store.getState());
+});
+
+store.dispatch(bugAdded('Bug 1'));
+store.dispatch(bugResolved(1));
+store.dispatch(bugRemoved(1));
+
+unsubscribe();
---
+++ src/reducer.js
@@ -0,0 +1,26 @@
+import * as actions from './actionTypes';
+// []
+
+let lastId = 0;
+
+const reducer = (state = [], action) => {
+  if (action.type === actions.BUG_ADDED)
+    return [
+      ...state,
+      {
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false
+      }
+    ];
+  else if (action.type === actions.BUG_REMOVED)
+    return state.filter(bug => bug.id !== action.payload.id);
+  else if (action.type === actions.BUG_RESOLVED)
+    return state.map(bug =>
+      bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
+    );
+
+  return state;
+};
+
+export default reducer;
