5bd8539a4a55a1fae3a1751476e6349ad945e117
Shivam Kumar <shivam66.sk@gmail.com>
2021-01-31 20:13:04 +0530

    writing clean redux code completed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import configureStore from './store/configureStore';
 import { bugAdded, bugRemoved, bugResolved } from './store/bugs';
+import { projectAdded } from './store/projects';
 
 const store = configureStore();
 
 const unsubscribe = store.subscribe(() => {
   console.log('Store Changed: ', store.getState());
 });
-
-store.dispatch(bugAdded('Bug 1'));
-store.dispatch(bugResolved(1));
-store.dispatch(bugRemoved(1));
+store.dispatch(projectAdded({ name: 'Project 1' }));
+store.dispatch(bugAdded({ description: 'Bug 1' }));
+store.dispatch(bugResolved({ id: 1 }));
+store.dispatch(bugRemoved({ id: 1 }));
 
 unsubscribe();
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,51 +1,30 @@
-// Action Types
-const BUG_ADDED = 'bugAdded';
-const BUG_REMOVED = 'bugRemoved';
-const BUG_RESOLVED = 'bugResolved';
+import { createSlice } from '@reduxjs/toolkit';
 
-// Action Creators
-export const bugAdded = description => ({
-  type: BUG_ADDED,
-  payload: {
-    description
-  }
-});
-
-export const bugRemoved = id => ({
-  type: BUG_REMOVED,
-  payload: {
-    id
-  }
-});
-
-export const bugResolved = id => ({
-  type: BUG_RESOLVED,
-  payload: {
-    id
-  }
-});
-
-// Reducer
 let lastId = 0;
 
-const reducer = (state = [], action) => {
-  if (action.type === BUG_ADDED)
-    return [
-      ...state,
-      {
+const slice = createSlice({
+  name: 'bugs',
+  initialState: [],
+  reducers: {
+    bugAdded: (bugs, action) => {
+      bugs.push({
         id: ++lastId,
         description: action.payload.description,
         resolved: false
-      }
-    ];
-  else if (action.type === BUG_REMOVED)
-    return state.filter(bug => bug.id !== action.payload.id);
-  else if (action.type === BUG_RESOLVED)
-    return state.map(bug =>
-      bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
-    );
+      });
+    },
 
-  return state;
-};
+    bugRemoved: (bugs, action) => {
+      const index = bugs.findIndex(bug => bug.id === action.payload.id);
+      bugs.splice(index, 1);
+    },
 
-export default reducer;
+    bugResolved: (bugs, action) => {
+      const index = bugs.findIndex(bug => bug.id === action.payload.id);
+      bugs[index].resolved = true;
+    }
+  }
+});
+
+export const { bugAdded, bugRemoved, bugResolved } = slice.actions;
+export default slice.reducer;
